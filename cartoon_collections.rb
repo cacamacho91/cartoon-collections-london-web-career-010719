ced670731e4db425c3d08047d7442643bc47fd1f
dashing-frame-3501 <cacamacho91@gmail.com>
2018-11-26 17:26:07 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,6 @@ def summon_captain_planet(planeteer_calls)
   excited_planeteer_calls = planeteer_calls.collect do |call|
     call.capitalize << "!"
   end
-  excited_planeteer_calls
 end
 
 def long_planeteer_calls# code an argument here
