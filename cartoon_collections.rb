c34d57e227778811e2db8892168e27e494baa4e6
dashing-frame-3501 <cacamacho91@gmail.com>
2018-11-26 17:29:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,9 @@ def long_planeteer_calls(long_planeteer_calls)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |potential_cheese|
+    cheese_types.include?(potential_cheese)
+  end
 end
